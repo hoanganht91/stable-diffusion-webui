f882c374c62e893185e677db68c26ba3d147c8c6
an.nguyen <hoanganht91@gmail.com>
2024-10-11 08:32:19 +0000

    Update init script for GCP

--- setup-init-gcp.sh
+++ setup-init-gcp.sh
@@ -2,14 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo apt update -y
+sudo apt update -y && sudo apt upgrade -y
 cd $SCRIPT_DIR
 
 # install python lib
 sudo apt install -y aria2 lnav jq yq
 sudo apt install -y libgoogle-perftools4 libtcmalloc-minimal4
-
-pip install --upgrade pip
 
 # create /workspace/logs if not exist
 mkdir -p /workspace/logs
